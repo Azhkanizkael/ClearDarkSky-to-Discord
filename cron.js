c15b28a1972020c285a346e3e0207c1966f5a538
Darrin Olsen <dolsen@cmnhospitals.org>
2023-01-13 16:40:26 -0700

    Added node-cron and a cron to handle subscriptions

---
+++ cron.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const cron = require('node-cron');
+const auth = require('./config.json');
+const fs = require('fs');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Post @ 6am every day
+cron.schedule('0 0 6 * * *', function() {
+	console.log('creating posts!');
+	fs.readFile('./commands/data/subscriptions.json', 'utf8', (err, data) => {
+		if (err) throw err;
+		const dataObject = JSON.parse(data);
+		// console.log(dataObject);
+		for (let i = 0; i < dataObject.length; i++) {
+			const subscribedGuild = client.guilds.cache.get(dataObject[i].guildId);
+			const subscribedChannel = client.channels.cache.get(dataObject[i].channelId);
+			console.log(`posting ${dataObject[i].URI} to ${subscribedGuild.name} in ${subscribedChannel.name}`);
+			subscribedChannel.send(dataObject[i].URI);
+		}
+	});
+}, {
+	timezone: 'America/Denver',
+});
+
+client.login(auth.token);
