af82a8f130dbc1a4a45a0bee47baf6c4ee1b5314
Darrin Olsen <dolsen@cmnhospitals.org>
2023-01-14 10:52:41 -0700

    downgrade node-cron to 3.0.0

--- cron.js
+++ cron.js
@@ -5,24 +5,22 @@ const fs = require('fs');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-	// Post @ 6am every day
-	cron.schedule('0 0 6 * * *', function() {
-		console.log('creating posts!');
-		fs.readFile('./commands/data/subscriptions.json', 'utf8', (err, data) => {
-			if (err) throw err;
-			const dataObject = JSON.parse(data);
-			// console.log(dataObject);
-			for (let i = 0; i < dataObject.length; i++) {
-				const subscribedGuild = client.guilds.cache.get(dataObject[i].guildId);
-				const subscribedChannel = client.channels.cache.get(dataObject[i].channelId);
-				console.log(`posting ${dataObject[i].URI} to ${subscribedGuild.name} in ${subscribedChannel.name}`);
-				subscribedChannel.send(dataObject[i].URI);
-			}
-		});
-	}, {
-		timezone: 'America/Denver',
+// Post @ 6am every day
+cron.schedule('0 0 6 * * *', function() {
+	console.log('creating posts!');
+	fs.readFile('./commands/data/subscriptions.json', 'utf8', (err, data) => {
+		if (err) throw err;
+		const dataObject = JSON.parse(data);
+		// console.log(dataObject);
+		for (let i = 0; i < dataObject.length; i++) {
+			const subscribedGuild = client.guilds.cache.get(dataObject[i].guildId);
+			const subscribedChannel = client.channels.cache.get(dataObject[i].channelId);
+			console.log(`posting ${dataObject[i].URI} to ${subscribedGuild.name} in ${subscribedChannel.name}`);
+			subscribedChannel.send(dataObject[i].URI);
+		}
 	});
+}, {
+	timezone: 'America/Denver',
 });
 
 client.login(auth.token);
